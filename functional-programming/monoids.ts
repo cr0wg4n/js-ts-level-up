fb1a1495480316ee7d5fdfe5456a6799e7caa59f
Mauricio M <mcm.crw@gmail.com>
2024-01-10 22:34:30 -0400

    feat: reduced example about monoids

--- functional-programming/monoids.ts
+++ functional-programming/monoids.ts
@@ -1,15 +1,20 @@
 import { Fold, Max, Min, Product, Sum} from './lib'
 
 const sumOfAll = [1,2,3,4,5].reduce(Sum.concat, Sum.empty())
-console.log(sumOfAll);
-console.log(Product.concat(Product.empty(), 5));
+console.log('sum all alternative way: ', sumOfAll);
+console.log('just sum:', Product.concat(Product.empty(), 5));
 
 const sumFold = Fold([1,2,3,4,5], Sum)
-console.log(sumFold);
+console.log('sum:', sumFold);
 
 const maxValue = Fold([1,2,3,4,5], Max)
-console.log(maxValue);
+console.log('max:', maxValue);
 
+const maxValueAlternative = [1,2,3,4,5].reduce(Max.concat, Max.empty())
+console.log('max alternative way, value:', maxValueAlternative)
 
 const minValue = Fold([0, 1,2,3,4,5], Min)
-console.log(minValue)
+console.log('min:', minValue)
+
+const minValueAlternative = [0, 1,2,3,4,5].reduce(Min.concat, Min.empty())
+console.log('min alternative way, value:', minValueAlternative)
