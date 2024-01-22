be8394162b27437ca51695225ebf6b84765c4ada
Mauricio M <mcm.crw@gmail.com>
2024-01-21 23:33:10 -0400

    feat: pipe and compose

--- functional-programming/pipelines/index.ts
+++ functional-programming/pipelines/index.ts
@@ -1,10 +1,16 @@
 const pipe = (...funcs) => x => funcs.reduce((v, f) => f(v), x)
+const compose = (...funcs) => x => funcs.reduceRight((v, f) => f(v), x)
+
 const repeat = value => `${value} ${value}`
 const capitalize = value => value[0].toUpperCase() + value.substring(1)
 const question = value => `${value}?`
 
 const result = question(capitalize(repeat('demo cocha')))
-const result2 = pipe(repeat, capitalize, question)('demo cocha')
+const resultPipe = pipe(repeat, capitalize, question)('demo cocha')
+// Compose is the same as pipe, but inversed (from right to left)
+const resultCompose = compose(question, capitalize, repeat)('demo cocha')
 
 console.log(result);
-console.log(result2);
+console.log(resultPipe);
+console.log(resultCompose);
+
